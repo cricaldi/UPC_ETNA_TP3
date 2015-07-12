691456d9df6e920f8b61d7825af827a438c79335
Luis Polo <i820326@gmail.com>
2015-07-12 09:41:24 -0500

    Fabricacion lista de solicitudes pendientes

--- Proyecto/ETNA/ETNA.MVC/Controllers/FB/SolicitudProduccionController.cs
+++ Proyecto/ETNA/ETNA.MVC/Controllers/FB/SolicitudProduccionController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using AutoMapper;
+using ETNA.MVC.FabricacionServices;
+using ETNA.MVC.Models.FB;
 namespace ETNA.MVC.Controllers.FB
 {
     public class SolicitudProduccionController : Controller
@@ -13,7 +15,13 @@ namespace ETNA.MVC.Controllers.FB
 
         public ActionResult Index()
         {
-            return View();
+            var service = new FabricacionServices.FabricacionServiceClient();
+            List<TB_PR_SolicitudProduccion> listadoSolicitud = service.TB_PR_SolicitudProduccion_Listar("1").ToList();
+
+            Mapper.CreateMap<TB_PR_SolicitudProduccion, SolicitudProduccionViewModel>();
+            var model = Mapper.Map<List<SolicitudProduccionViewModel>>(listadoSolicitud);
+
+            return View(model);
         }
 
     }
--- Proyecto/ETNA/ETNA.MVC/Models/FB/SolicitudProduccionViewModel.cs
+++ Proyecto/ETNA/ETNA.MVC/Models/FB/SolicitudProduccionViewModel.cs
@@ -9,7 +9,23 @@ namespace ETNA.MVC.Models.FB
 {
     public class SolicitudProduccionViewModel
     {
-        [DisplayName("Descripcion")]
+        [DisplayName("Código solicitud")]
+        public string SolicitudProdId { get; set; }
+
+        [DisplayName("Descripción")]
         public string Descripcion { get; set; }
+
+        [DisplayName("Fecha de solicitud")]
+        public DateTime FecSol { get; set; }
+
+        [DisplayName("Cant. personal")]
+        public int CantPersonal { get; set; }
+
+        [DisplayName("Cant. insumos")]
+        public int CantInsumos { get; set; }
+
+        [DisplayName("Cant. maquinarias")]
+        public int CantMaquinaria { get; set; }
+
     }
 }
