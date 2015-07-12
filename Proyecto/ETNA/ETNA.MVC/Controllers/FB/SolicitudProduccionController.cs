4b1e1826872b8f1325dd05b2652ec08d637978b6
Luis Polo <i820326@gmail.com>
2015-07-12 02:37:00 -0500

    Fabricacion Listado de solicitudes

---
+++ Proyecto/ETNA/ETNA.MVC/Controllers/FB/SolicitudProduccionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETNA.MVC.Controllers.FB
+{
+    public class SolicitudProduccionController : Controller
+    {
+        //
+        // GET: /SolicitudProduccion/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Proyecto/ETNA/ETNA.MVC/Models/FB/SolicitudProduccionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ETNA.MVC.Models.FB
+{
+    public class SolicitudProduccionViewModel
+    {
+        [DisplayName("Descripcion")]
+        public string Descripcion { get; set; }
+    }
+}
